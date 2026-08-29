f44bf86a900ca5739244976b10d7a9d6199966c1
bhattibhers776-glitch <bhattibhers776@gmail.com>
2026-08-29 15:58:23 +0500

    e

--- script.js
+++ script.js
@@ -4,29 +4,34 @@ let intro = document.querySelector('.intro');
 let logo = document.querySelector('.logo-header');
 let logoSpan = document.querySelectorAll('.intro-logo');
 
-window.addEventListener('DOMContentLoaded', ()=>{
-    setTimeout(()=>{
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
 
-        logoSpan.forEach((span, idx)=>{
-            setTimeout(()=>{
+        // Logo letters animation
+        logoSpan.forEach((span, idx) => {
+            setTimeout(() => {
                 span.classList.add('active');
-            }, (idx+1)*150)
+            }, (idx + 1) * 150);
         });
 
-        setTimeout(()=>{
-            logoSpan.forEach((span,idx)=>{
-                setTimeout(()=>{
+        // Logo fade animation
+        setTimeout(() => {
+            logoSpan.forEach((span, idx) => {
+                setTimeout(() => {
                     span.classList.remove('active');
                     span.classList.add('fade');
-                }, (idx+1)*50)
-            })
-        },2000)
+                }, (idx + 1) * 50);
+            });
+        }, 3500);
 
-        setTimeout(()=>{
-            intro.style.top='-100vh';
-        },2300)
-    })
-})
+        // Hide intro screen
+        setTimeout(() => {
+            intro.style.top = '-100vh';
+        }, 7000);
+
+    });
+});
+
 
 // Scroll to top upon reload
 
@@ -34,11 +39,13 @@ window.onload = function() {
     window.scrollTo(0, 0);
 };
 
+
 // Hamburger navbar
 
-function togglemenu(){
+function togglemenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
+
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 
@@ -50,15 +57,16 @@ function togglemenu(){
     }
 }
 
+
 function closeMenuOnClickOutside(event) {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
 
-    // Check if the click is outside the menu and the hamburger icon
+    // Check if the click is outside the menu and hamburger icon
     if (!menu.contains(event.target) && !icon.contains(event.target)) {
         menu.classList.remove("open");
         icon.classList.remove("open");
-        
+
         // Remove the event listener after closing the menu
         document.removeEventListener("click", closeMenuOnClickOutside);
     }
@@ -67,29 +75,33 @@ function closeMenuOnClickOutside(event) {
 
 // Into fade-blur transition
 
-const observer = new IntersectionObserver((entries)=>{
-    entries.forEach((entry)=>{
-        console.log(entry)
-        if (entry.isIntersecting){
-            entry.target.classList.add('show')
-        } 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
     });
 });
 
+
 const hiddenElements = document.querySelectorAll('.section__text');
-hiddenElements.forEach((el)=>observer.observe(el));
+
+hiddenElements.forEach((el) => observer.observe(el));
+
 
 // Navbar turns black on scroll
 
 // function changeBG() {
 //     var navbar = document.getElementById('desktop-nav');
 //     var scrollValue = window.scrollY;
-//     if(scrollValue < 25) {
-//         navbar.classList.remove('bgcolor')
+
+//     if (scrollValue < 25) {
+//         navbar.classList.remove('bgcolor');
 //     } else {
-//         navbar.classList.add('bgcolor')
+//         navbar.classList.add('bgcolor');
 //     }
 // }
 
-// window.addEventListener('scroll', changeBG)
-
+// window.addEventListener('scroll', changeBG);
